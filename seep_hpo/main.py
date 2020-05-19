024993594614ddac4cd48f14219cb796b29c640b
Michael Gargano <Michael.Gargano@jax.org>
2020-05-19 09:37:29 -0400

    adding click to requirements, removing comments

--- seep_hpo/main.py
+++ seep_hpo/main.py
@@ -1,10 +1,5 @@
-#import logging
 import sys
 import click
-#DEFAULT_LOG_FMT = '%(asctime)s %(name)-20s %(levelname)-3s : %(message)s'
-#logging.basicConfig(level=logging.INFO, format=DEFAULT_LOG_FMT)
-#logger = logging.getLogger('seep_hpo')
-#logger.info("Fake Version Here")
 
 
 @click.group()
