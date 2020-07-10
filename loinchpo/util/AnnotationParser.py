11e9c33276e29346e5156c3b107510c306802752
Michael Gargano <Michael.Gargano@jax.org>
2020-07-10 16:31:05 -0400

    Adding in an annotation parser for a pandas dataframe with test and updating requirments, conda_recipe update, deploying to pypi as 1.02

--- loinchpo/util/AnnotationParser.py
+++ loinchpo/util/AnnotationParser.py
@@ -143,4 +143,80 @@ class AnnotationParser:
         except (OSError,  SeepParsingError, SeepValidationError) as e:
             raise e
 
+    @staticmethod
+    def parse_annotation_pandas_dict(df):
+        """ Parsing Loinc2HpoAnnotation pandas to a traversable dictionary
+
+            This method has a specific return of a dictionary of dictionaries to help us query or
+            traverseannotations when resolving input queries from users.
+
+        Args:
+            df: A pandas dataframe with loinc queries. (loincId, loincMeasure, negated)
+
+        Returns:
+            A dictionary of dictionaries with a specific mapping to help us query for
+            loinc code, measures and negations to get us an hpo code.
+
+            {'29298-0': {'POS': {'N': 'HP:0009281'}}}
+
+            In this example we see that a loinc_id has a measure of positive and it is not negated
+            which results in an hpo code.
+
+        Raises:
+            OSError: When the file could not be opened or found.
+            SeepParsingError: When there were issues with file format
+            SeepValidationError: When there were issues with specific field expected values.
+        """
+        annotations = {}
+        expected_fields = ("loincId", "loincScale",	"system", "code", "hpoTermId", "isNegated", "createdOn", 
+        "createdBy", "lastEditedOn", "lastEditedBy", "version", "isFinalized", "comment")
+        try:
+            cols = df.columns
+            if not all(col in expected_fields for col in cols):
+                raise SeepParsingError("Dataframe columns not correct, must be (loincId	loincScale	system	code " + 
+                    "hpoTermId	isNegated	createdOn	createdBy	lastEditedOn	lastEditedBy	version	isFinalized	comment)")
+            for row in df.iterrows():
+                loinc_id, loinc_scale, system, measure, hpo_term, is_negated, created_on, \
+                created_by, last_edit_date, last_edit_by, version, finalized, comment = row[1].tolist()
+
+                # Sanity Check loinc_id and other things like in the class.
+                AnnotationUtility.check_all(loinc_id, measure)
+                # Parse Negation
+                is_negated = AnnotationUtility.interpret_negated(loinc_id, is_negated)
+                # Map the Loinc Scale
+                loinc_scale = LoincScale.map_loinc_scale(loinc_scale)
+
+                # Build named tuple if we are okay. Append to list.
+                annotation = LoincHpoAnnotation(loinc_id, loinc_scale, system, measure,
+                                                hpo_term, is_negated, created_on,
+                                                created_by, last_edit_date, last_edit_by,
+                                                version, finalized, comment)
+                if loinc_id in annotations:
+                    if annotation in annotations[loinc_id]:
+                        raise SeepParsingError("Duplicate annotation exists for loinc code.")
+                    else:
+                        annotations[loinc_id].append(annotation)
+                else:
+                    annotations[loinc_id] = [annotation]
+            # remapping to a queryable dictionary given specific outcomes.
+            # loinc_id -> scale -> measure -> negated -> term
+            remapped_annotations = {}
+            for loinc_id in annotations:
+                # get the list
+                annotation_list = annotations[loinc_id]
+                for annotation in annotation_list:
+                    if annotation.loinc_id not in remapped_annotations:
+                        remapped_annotations[annotation.loinc_id] = {annotation.measure: {annotation.is_negated: annotation.hpo_term}}
+                    else:
+                        if annotation.measure not in remapped_annotations[annotation.loinc_id]:
+                            remapped_annotations[annotation.loinc_id][annotation.measure] = \
+                                {annotation.is_negated: annotation.hpo_term}
+                        else:
+                            if is_negated not in \
+                                    remapped_annotations[annotation.loinc_id][annotation.measure]:
+                                remapped_annotations[annotation.loinc_id][annotation.measure][is_negated] = annotation.hpo_term
+            return remapped_annotations
+        except (OSError,  SeepParsingError, SeepValidationError) as e:
+            raise e
+
 
--- loinchpo/util/tests/test_annotation_parser.py
+++ loinchpo/util/tests/test_annotation_parser.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from ddt import ddt, data
+import pandas as pd
 from loinchpo.util.AnnotationParser import AnnotationParser
 
 
@@ -31,6 +32,35 @@ class AnnotationParserTest(unittest.TestCase):
         test_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                  'test_annotation_file.tsv')
         annotations = AnnotationParser.parse_annotation_file_dict(test_file)
+        for expected in expected_data:
+            key = next(iter(expected))
+            self.assertEqual(expected[key], annotations[key])
+
+    @data(
+        ({
+         "loincId": ["2823-3", "2823-3", "5803-2", "2091-7"],
+         "loincScale": ["Qn","Qn","Qn","Qn"],
+         "system": ["FHIR", "FHIR", "FHIR","FHIR"],
+         "code": ["N", "H", "H", "H"],
+         "hpoTermId": ["HP:0011042", "HP:0011042", "HP:0032369", "HP:0003362"],
+         "isNegated": [True, True, False, False],
+         "createdOn": ["08-102-303982", "08-102-303982", "08-102-303982", "08-102-303982"],
+         "createdBy": ["me", "me" ,"me" ,"me"],
+         "lastEditedOn": ["NA","NA","NA","NA"],
+         "lastEditedBy": ["X", "X", "X", "X"],
+         "version": [1, 1, 1, 1],
+         "isFinalized": [True, True, True, True],
+         "comment": ["", "", "", ""]
+        })
+    )
+    def test_annotation_pandas_dict(self, input_frame):
+        expected_data = ({"2823-3": {"N": {True: "HP:0011042"}, "H": {True: "HP:0011042"}}},
+           {"5803-2": {"H": {False: "HP:0032369"}}},
+           {"2091-7": {"H": {False: "HP:0003362"}}})
+        frame = pd.DataFrame(input_frame, columns=['loincId', 'loincScale',
+        'system', 'code', 'hpoTermId', 'isNegated', 'createdOn', 'createdBy', 'lastEditedOn',
+        'lastEditedBy', 'version', 'isFinalized', 'comment'])
+        annotations = AnnotationParser.parse_annotation_pandas_dict(frame)
         for expected in expected_data:
             key = next(iter(expected))
             self.assertEqual(expected[key], annotations[key])
