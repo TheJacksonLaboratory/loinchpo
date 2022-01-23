f0f7a5f448d748bd5113b3c5423603689be37fad
iimpulse <michael.gargano@jax.org>
2022-01-22 23:59:45 -0700

    updating supporting files

--- loinchpo/__init__.py
+++ loinchpo/__init__.py
@@ -4,5 +4,6 @@ __version__ = pkg_resources.get_distribution('loinchpo').version
 from .util.AnnotationResolver import AnnotationResolver
 from .util.AnnotationParser import AnnotationParser
 from .util.QueryFileParser import QueryFileParser
+from .util.AnnotationUtility import AnnotationUtility
 from .models.Query import Query
 from .models.LoincId import LoincId
