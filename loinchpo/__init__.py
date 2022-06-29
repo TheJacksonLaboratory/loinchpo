a96f8499416e8de8cf5c8de10ab225f8d80f7ecb
iimpulse <michael.gargano@jax.org>
2022-06-29 16:28:35 -0600

    this could be a version issue

--- loinchpo/__init__.py
+++ loinchpo/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution('loinchpo').version
+__version__ = pkg_resources.require('loinchpo')[0].version
 from .io.QueryResolver import QueryResolver
 from .io.AnnotationParser import AnnotationParser
 from .io.QueryFileParser import QueryFileParser
