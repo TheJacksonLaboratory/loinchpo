c7512e2f9b4725053c4b20e5c9517e8d13d22a80
iimpulse <michael.gargano@jax.org>
2022-06-29 17:00:01 -0600

    static method for loinc id

--- loinchpo/model/LoincId.py
+++ loinchpo/model/LoincId.py
@@ -9,7 +9,8 @@ class LoincId:
     def __init__(self, loinc_id):
         self.num, self.suffix, self.loinc_id = self.parse_loinc_id(loinc_id)
 
-    def parse_loinc_id(self, loinc_id):
+    @staticmethod
+    def parse_loinc_id(loinc_id):
         try:
             num, suffix = loinc_id.split("-")
             return num, suffix, loinc_id
